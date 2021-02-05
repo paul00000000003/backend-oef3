e6fd1da84370cf7c9961d7eeed4a11e3d2635ad5
PaulvanMierlo <pauljozefvanmierlo@yahoo.com>
2021-02-05 10:25:08 +0100

    wijzigingen

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+# Create a Flask app inside `app`
+app = Flask(__name__)
+
+# Assign a function to be called when the path `/` is requested
+
+
+@app.route('/')
+def index():
+    return 'Hello, world!'
+
+
+@app.route('/cow')
+def cow():
+    return 'MOoooOo!'
---
+++ main_test.py
@@ -0,0 +1,11 @@
+# test_hello.py
+from main import app
+
+
+def test_main():
+    response = app.test_client().get('/')
+    assert response.status_code == 200
+    assert response.data == b'Hello, world!'
+    response = app.test_client().get('/cow')
+    assert response.status_code == 200
+    assert response.data == b'MOoooOo!'
